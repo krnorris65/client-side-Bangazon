4d39659e17c4ec7f3d360c64b695a9b01a15801e
Kristen Norris <krnorris65@gmail.com>
2018-01-30 17:20:41 -0600

    converting to angular

---
+++ app/Customer/factory/CustomerFactory.js
@@ -0,0 +1,48 @@
+angular.module("Bangazon")
+.factory("CustomerFactory", function($http){
+
+    return Object.create(null, {
+        "allCustomers": {
+            value: function () {
+                return $http({
+                    "url": "http://localhost:5000/api/customer",
+                    "method": "GET"
+                })
+
+            }
+        },
+        "single": {
+            value: function (id) {
+                return $http({
+                    "url": `http://localhost:5000/api/customer/${id}`,
+                    "method": "GET"
+                })
+
+            }
+        },
+        "update": {
+            value: function (customerInfo, id) {
+                return $http({
+                    "url": `http://localhost:5000/api/customer/${id}`,
+                    "method": "PUT",
+                    "data": JSON.stringify(customerInfo),
+                    "headers": {
+                        "Content-Type": "application/json"
+                    }
+                })
+            }
+        },
+        "add": {
+            value: function (customerInfo) {
+                return $http({
+                    "url": "http://localhost:5000/api/customer",
+                    "method": "POST",
+                    "data": JSON.stringify(customerInfo),
+                    "headers": {
+                        "Content-Type": "application/json"
+                    }
+                })
+            }
+        }
+    })
+})
---
+++ app/app.js
@@ -0,0 +1,12 @@
+angular.module("Bangazon", ["ngRoute"])
+
+angular.module("Bangazon").config(function ($routeProvider) {
+	/**
+     * Configure all Angular application routes here
+     */
+	$routeProvider.
+		when("/customer", {
+			templateUrl: "app/Customer/partials/customer.html",
+			controller: "CustomerCtrl"
+        })
+    })
